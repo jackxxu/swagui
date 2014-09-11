44f5fee4d35c194e23dca9051f776b4189dd5dc2
Jack Xu <jackxxu@gmail.com>
2014-09-10 22:31:49 -0400

    to enable .yml and .json doc formats

--- lib/swagui/middleware.rb
+++ lib/swagui/middleware.rb
@@ -1,4 +1,6 @@
 require 'rack'
+require 'json'
+require 'yaml'
 
 module Swagui
 
@@ -20,18 +22,35 @@ module Swagui
 
     def call(env)
       path = env["PATH_INFO"].strip
-
-      if @assets_url_regex.match(path)
+      if @assets_url_regex.match(path) # for assets (css/js) files
         env["PATH_INFO"] = path.gsub(@assets_url_regex, '')
         @swagger_ui_file_server.call(env)
       elsif @url_regex.match(path)
-        path = path.gsub(@url_regex, '')
+        path = path.gsub(@url_regex, '') # root path renders index.html
         if path == '' || path == '/'
           env["PATH_INFO"] = 'index.html'
           @swagger_ui_file_server.call(env)
         else
-          env["PATH_INFO"] = path
-          @app_file_server.call(env)
+
+          env['HTTP_IF_MODIFIED_SINCE'] = nil # not 304s
+
+          response = [404, {"Content-Type"=>"application/json"}, '']
+          extension = ['', '.json', '.yml'].find do |ext|
+            response = @app_file_server.call(env.dup.merge!('PATH_INFO' => "#{path}#{ext}"))
+            response[0] == 200
+          end
+
+          # handles yaml parsing
+          if extension == '.yml'
+            body = ''
+            response[2].each {|f| body = YAML::load(f).to_json}
+            response[2] = [body]
+            response[1].merge!("Content-Length"=> body.size.to_s)
+          end
+
+          response[1].merge!("Content-Type"=>"application/json") # response is always json content
+
+          response
         end
       else
         @app.call(env)
